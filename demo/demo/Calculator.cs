2522dba8a0499e3eb41bb3da471078a75a56388a
Junhan Ma <ma.jun@husky.neu.edu>
2019-07-08 21:55:38 -0400

    cal demo

---
+++ demo/demo/Calculator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+namespace demo
+{
+//    public abstract class Calculator<T>
+//    {
+        
+
+//    public abstract T Add(T a, T b);
+    
+//}
+
+    //class ScientificCalculator: Calculator<int> { 
+
+    //    public override int Add(int a, int b) {
+    //        return a + b;
+    //    }
+    //}
+
+    //class ComplexCalculator: Calculator<Complex> { 
+    //    public override Complex Add(Complex a, Complex b) {
+    //        Complex res = new Complex();
+    //        res.re = a.re + b.re;
+    //        res.im = a.im + b.im;
+
+    //        return res;
+    //    }
+    
+    //}
+
+
+        //public class Calculator {
+        //public IAddition addition;
+
+        //public Calculator(IAddition i) {
+        //    addition = i;
+        //}
+
+        //public void Add() { 
+        //    addition.
+        //}
+        //}
+    public abstract class Calculator<T> {
+
+        public abstract T Add(T a, T b); 
+    }
+
+    class Complex
+    {
+        public int re;
+        public int im;
+
+        public Complex() { }
+
+    }
+
+    public interface IAddition<T> {
+        T Addition(T a, T b);
+    }
+
+    class ScientificCalculator : Calculator<int>, IAddition<int> { 
+
+        public int Addition(int a, int b) {
+            return a + b; 
+        }
+
+        public override int Add(int a , int b)
+        {
+            return Addition(a, b);
+        }
+ 
+    }
+
+    class ComplexCalculator : Calculator<Complex>, IAddition<Complex> {
+
+        public Complex Addition(Complex a, Complex b) {
+            Complex res = new Complex();
+            res.re = a.re + b.re;
+            res.im = a.im + b.im;
+
+            return res;
+        }
+
+        public override Complex Add(Complex a, Complex b)
+        {
+            return Addition(a, b);
+        }
+    }
+
+
+
+}
---
+++ demo/demo/Complex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace demo
+{
+    public class Complex
+    {
+        public int re;
+        public int im;
+
+        Complex() { }
+
+    }
+}
