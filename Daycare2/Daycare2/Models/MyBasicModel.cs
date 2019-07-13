28e12f4441fa48d378a7de4929c0958ca9ca47ff
Junhan Ma <ma.jun@husky.neu.edu>
2019-07-13 15:00:57 -0400

    daycare web

---
+++ Daycare2/Daycare2/Models/MyBasicModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Daycare2.Models
+{
+    public class MyBasicModel
+    {
+        public string Name { set; get; }
+
+        public MyBasicModel()
+        {
+        }
+    }
+}
